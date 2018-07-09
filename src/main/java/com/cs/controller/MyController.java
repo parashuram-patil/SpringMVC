6f356be0bf2849269eef1a60666b035f2a07a3e3
parashuram@contentsphere.com <Parashuram.Patil@DP213.contentserv.com>
2018-07-09 17:27:10 +0530

    POST Request Implementation -- Input POST URL : http://localhost:8091/SpringMVC/postRequest/123 Pay Load : { "fname" : "Parashuram", "lname" : "Patil" }

--- src/main/java/com/cs/controller/MyController.java
+++ src/main/java/com/cs/controller/MyController.java
@@ -3,7 +3,11 @@ package com.cs.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.cs.model.MyModel;
 
 @Controller
 public class MyController {
@@ -16,6 +20,17 @@ public class MyController {
     System.out.println("**********************");
     
     return id + 100;
+  }
+  
+  @PostMapping("postRequest/{id}")
+  public @ResponseBody MyModel handleRequest(@PathVariable Integer id, @RequestBody MyModel model)
+  {
+    
+    System.out.println("**********************");
+    System.out.println(id + "::" + model.getFname() + "::" + model.getLname());
+    System.out.println("**********************");
+    
+    return model;
   }
   
 }
---
+++ src/main/java/com/cs/model/MyModel.java
@@ -0,0 +1,24 @@
+package com.cs.model;
+
+public class MyModel {
+
+	protected String fname;
+	protected String lname;
+
+	public String getFname() {
+		return fname;
+	}
+
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
+	public String getLname() {
+		return lname;
+	}
+
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+
+}
