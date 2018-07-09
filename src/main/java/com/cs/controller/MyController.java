aeef937455577bdd9820b827277a101c13240d63
parashuram@contentsphere.com <Parashuram.Patil@DP213.contentserv.com>
2018-07-09 17:43:35 +0530

    PUT adn DELETE Request Implementation

--- src/main/java/com/cs/controller/MyController.java
+++ src/main/java/com/cs/controller/MyController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.cs.model.MyModel;
@@ -13,7 +15,7 @@ import com.cs.model.MyModel;
 public class MyController {
   
   @GetMapping("getRequest/{id}")
-  public @ResponseBody Integer handleRequest(@PathVariable Integer id)
+  public @ResponseBody Integer handleGetRequest(@PathVariable Integer id)
   {
     System.out.println("**********************");
     System.out.println(id);
@@ -23,7 +25,7 @@ public class MyController {
   }
   
   @PostMapping("postRequest/{id}")
-  public @ResponseBody MyModel handleRequest(@PathVariable Integer id, @RequestBody MyModel model)
+  public @ResponseBody MyModel handlePostRequest(@PathVariable Integer id, @RequestBody MyModel model)
   {
     
     System.out.println("**********************");
@@ -32,5 +34,28 @@ public class MyController {
     
     return model;
   }
+  
+  @RequestMapping(value = "/putRequest/{id}", method = {RequestMethod.PUT })
+  public @ResponseBody MyModel handlePutRequest(@PathVariable Integer id, @RequestBody MyModel model)
+  {
+    
+    System.out.println("**********************");
+    System.out.println(id + "::" + model.getFname() + "::" + model.getLname());
+    System.out.println("**********************");
+    
+    return model;
+  }
+  
+  @RequestMapping(value = "/deleteRequest/{id}", method = {RequestMethod.DELETE })
+  public @ResponseBody Integer handleDeleteRequest(@PathVariable Integer id)
+  {
+    
+    System.out.println("**********************");
+    System.out.println(id);
+    System.out.println("**********************");
+    
+    return id;
+  }
+  
   
 }
