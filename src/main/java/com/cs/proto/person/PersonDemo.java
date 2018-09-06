8b9fa6908d2e5c05e08e31c065a61ef2f595e89f
unknown <Parashuram.Patil@DP213-CS.contentserv.com>
2018-09-06 14:33:16 +0530

    Package and Import Protobufs

--- src/main/java/com/cs/proto/person/PersonDemo.java
+++ src/main/java/com/cs/proto/person/PersonDemo.java
@@ -3,6 +3,8 @@ package com.cs.proto.person;
 import java.io.IOException;
 
 import com.cs.proto.compile.person.PersonProto.Person;
+import com.cs.proto.compile.person.PhoneNumberProto.PhoneNumber;
+import com.cs.proto.compile.person.PhoneNumberProto.PhoneType;
 import com.google.protobuf.util.JsonFormat;
 
 public class PersonDemo {
@@ -13,6 +15,8 @@ public class PersonDemo {
     person.setAge(27);
     person.setEmail("a@a.a");
     person.setName("Psp");
+    person.addPhones(PhoneNumber.newBuilder().setNumber("123456789").setType(PhoneType.MOBILE));
+    person.addPhones(PhoneNumber.newBuilder().setNumber("987654321").setType(PhoneType.HOME));
 
     System.out.println(JsonFormat.printer().print(person));
     //JsonFormat format = new JsonFormat();
@@ -21,8 +25,15 @@ public class PersonDemo {
     String personString = "{\r\n" + 
         "  \"name\": \"Psp\",\r\n" + 
         "  \"email\": \"a@a.a\",\r\n" + 
-        "  \"age\": 27\r\n" + 
+        "  \"age\": 27,\r\n" + 
+        "  \"phones\": [{\r\n" + 
+        "    \"number\": \"123456789\"\r\n" + 
+        "  }, {\r\n" + 
+        "    \"number\": \"987654321\",\r\n" + 
+        "    \"type\": \"HOME\"\r\n" + 
+        "  }]\r\n" + 
         "}";
+    
     Person.Builder person1 = Person.newBuilder();
     JsonFormat.parser().merge(personString, person1);
     //InputStream inputStream = new ByteArrayInputStream(personString.getBytes(Charset.forName("UTF-8")));
